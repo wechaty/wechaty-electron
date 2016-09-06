22b1775cc5afcf26c49d362fb1e2f65c5377b701
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2016-09-07 01:32:05 +0800

    wechaty(webdriver) quit first, then electron quit

--- app/wechaty-client.js
+++ app/wechaty-client.js
@@ -1,26 +1,5 @@
 // const fs = require('fs')
 
-const Brolog = require('brolog')
-
-const { IoClient } = require('wechaty')
-
-// document.write(file)
-
-
-brolog.info('Wechaty', 'Wechaty EPP starting...')
-
-
-const client = new IoClient({
-  token: 'EPP'
-  , log: Brolog
-})
-
-client.init()
-    .then(_ => brolog.verbose('startIoBot()', 'init-ed'))
-    .catch(e => {
-      brolog.error('Bot', 'init() fail: %s', e.message)
-      throw e
-      // process.exit(-1)
-    })
+const client = null
 
 module.exports = client
